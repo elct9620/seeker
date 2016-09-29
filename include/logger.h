914b3093da2d041dad065b18a5334536417b352f
蒼時弦也 <elct9620@frost.tw>
2016-09-29 10:13:25 +0800

    Simplify logger

--- include/logger.h
+++ include/logger.h
@@ -5,12 +5,22 @@
 
 using std::string;
 
+#define LOG(priority, format, ...)              \
+  va_list args;                                 \
+  va_start(args, format);                       \
+  SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION, \
+                  SDL_LOG_PRIORITY_##priority,  \
+                  format.c_str(),               \
+                  args);                        \
+  va_end(args);
+
 class Logger {
   public:
     static void Verbose(string format, ...);
-    static void Info(string format, ...);
     static void Debug(string format, ...);
+    static void Info(string format, ...);
+    static void Warn(string format, ...);
     static void Error(string format, ...);
     static void Critical(string format, ...);
-    static void Warn(string format, ...);
 };
+
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -2,62 +2,26 @@
 
 #include "logger.h"
 
-void Logger::Info(string format, ...) {
-  va_list args;
-  va_start(args, format);
-  SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION,
-                  SDL_LOG_PRIORITY_INFO,
-                  format.c_str(),
-                  args);
-  va_end(args);
+void Logger::Verbose(string format, ...) {
+	LOG(VERBOSE, format)
 }
 
-void Logger::Verbose(string format, ...) {
-  va_list args;
-  va_start(args, format);
-  SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION,
-                  SDL_LOG_PRIORITY_VERBOSE,
-                  format.c_str(),
-                  args);
-  va_end(args);
+void Logger::Info(string format, ...) {
+	LOG(INFO, format)
 }
 
 void Logger::Debug(string format, ...) {
-  va_list args;
-  va_start(args, format);
-  SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION,
-                  SDL_LOG_PRIORITY_DEBUG,
-                  format.c_str(),
-                  args);
-  va_end(args);
+	LOG(DEBUG, format)
 }
 
 void Logger::Warn(string format, ...) {
-  va_list args;
-  va_start(args, format);
-  SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION,
-                  SDL_LOG_PRIORITY_WARN,
-                  format.c_str(),
-                  args);
-  va_end(args);
+	LOG(WARN, format)
 }
 
 void Logger::Error(string format, ...) {
-  va_list args;
-  va_start(args, format);
-  SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION,
-                  SDL_LOG_PRIORITY_ERROR,
-                  format.c_str(),
-                  args);
-  va_end(args);
+	LOG(ERROR, format);
 }
 
 void Logger::Critical(string format, ...) {
-  va_list args;
-  va_start(args, format);
-  SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION,
-                  SDL_LOG_PRIORITY_CRITICAL,
-                  format.c_str(),
-                  args);
-  va_end(args);
+	LOG(CRITICAL, format)
 }
