f67ab3e897bc8c3c9642f03c2ea2c42bb4941e3d
蒼時弦也 <elct9620@frost.tw>
2016-10-20 20:28:34 +0800

    Implement define class for script engine

--- include/Script/Engine.h
+++ include/Script/Engine.h
@@ -3,10 +3,13 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 
 #include "Seeker.h"
 
 #include "mruby.h"
+#include "mruby/data.h"
+#include "mruby/class.h"
 #include "mruby/string.h"
 #include "mruby/compile.h"
 
@@ -21,17 +24,36 @@ namespace Seeker {
       public:
         static Engine* instance();
 
+        static mrb_data_type* getDataType(string name);
+        static RClass* getClass(string name);
+
         void loadScript(const string &filename);
         void executeScript(const string &script);
         void captureException();
+
+        template<class T>
+        void defineClass(void (*callback)(RClass*));
+        template<class T>
+        void defineClass(void (*callback)(RClass*), RClass* parent);
+
+        void defineMethod(RClass* klass, mrb_func_t func, mrb_aspec aspec);
 
       private:
         Engine();
         ~Engine();
 
+        RClass* createClass(string name, RClass* parent);
+        mrb_data_type* createDataType(string name);
+
+        template<class T>
+        static mrb_value classInitializer(mrb_state* mrb, mrb_value self);
+
       private:
         static Engine* _instance;
         static string basePath;
+
+        static unordered_map<string, RClass*> definedClass;
+        static unordered_map<string, mrb_data_type> definedType;
 
         mrb_state* mrb;
     };
--- src/Script/Engine.cpp
+++ src/Script/Engine.cpp
@@ -5,6 +5,8 @@
 namespace Seeker {
   namespace Script {
     string Engine::basePath = string(SDL_GetBasePath());
+    unordered_map<string, RClass*> Engine::definedClass = {};
+    unordered_map<string, mrb_data_type> Engine::definedType = {};
 
     Engine::Engine() {
       mrb = mrb_open();
@@ -19,6 +21,24 @@ namespace Seeker {
         _instance = new Engine;
       }
       return _instance;
+    }
+
+    mrb_data_type* Engine::getDataType(string name) {
+      auto it = definedType.find(name);
+      if(it != definedType.end()) {
+        return &(*it).second;
+      }
+
+      return nullptr;
+    }
+
+    RClass* Engine::getClass(string name) {
+      auto it = definedClass.find(name);
+      if(it != definedClass.end()) {
+        return (*it).second;
+      }
+
+      return nullptr;
     }
 
     Engine* Engine::_instance = Engine::instance();
@@ -53,6 +73,72 @@ namespace Seeker {
         mrb_value exception = mrb_obj_value(mrb->exc);
         Logger::Error("Script => %s", mrb_string_value_ptr(mrb, exception));
       }
+    }
+
+    template<class T>
+    void Engine::defineClass(void(*body)(RClass*)) {
+      defineClass<T>(body, mrb->object_class);
+    }
+
+    template<class T>
+    void Engine::defineClass(void(*callback)(RClass*), RClass* parent) {
+      string className(typeid(T).name());
+      RClass* klass = createClass(className, parent);
+
+      // TODO: Provide custom initializer options
+      mrb_define_method(mrb, klass, "initialize", &Engine::classInitializer<T>, MRB_ARGS_NONE());
+
+      if(callback) {
+        callback(klass);
+      }
+    }
+
+    RClass* Engine::createClass(string name, RClass* parent) {
+      RClass* klass = getClass(name);
+
+      if(klass == nullptr) {
+        klass = mrb_define_class(mrb, name.c_str(), parent);
+        definedClass.insert(std::pair<string, RClass*>(name, klass));
+      }
+
+      return klass;
+    }
+
+    mrb_data_type* Engine::createDataType(string name) {
+      mrb_data_type* dataType = getDataType(name);
+      if(dataType) {
+        return dataType;
+      }
+
+      mrb_data_type newType = { name.c_str(), mrb_free };
+      definedType.insert(std::pair<string, mrb_data_type>(name, newType));
+
+      // Redo to get the pointer saved in unorederd map
+      return createDataType(name);
+    }
+
+    template<class T>
+    mrb_value Engine::classInitializer(mrb_state* mrb, mrb_value self) {
+      T* application;
+      application = (T*) DATA_PTR(self);
+
+      if(application != NULL) {
+        delete application;
+      }
+
+      string className(typeid(T).name());
+      mrb_data_type* dataType = getDataType(className) ;
+      if(dataType) {
+        DATA_TYPE(self) = dataType;
+      }
+
+      DATA_PTR(self) = NULL;
+
+      application = (T*) mrb_alloca(mrb, sizeof(T));
+
+      DATA_PTR(self) = application;
+
+      return self;
     }
   }
 }
